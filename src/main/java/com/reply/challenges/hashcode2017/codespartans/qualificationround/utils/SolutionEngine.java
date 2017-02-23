78681bea845f62b5ab0d9c6c6300c7a4ec9101b4
psychowood <psychowood@gmail.com>
2017-02-23 20:02:14 +0100

    Empty solution

--- src/main/java/com/reply/challenges/hashcode2017/codespartans/qualificationround/utils/SolutionEngine.java
+++ src/main/java/com/reply/challenges/hashcode2017/codespartans/qualificationround/utils/SolutionEngine.java
@@ -1,5 +1,9 @@
 package com.reply.challenges.hashcode2017.codespartans.qualificationround.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Cache;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.ProblemParameters;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Solution;
 
@@ -13,7 +17,15 @@ public class SolutionEngine {
 	public static Solution processSolution(ProblemParameters parameters) {
 		log.info("Start looking for a solution to problem {}", parameters);
 		
-		return null;
+		int cacheServersNumber = parameters.getCacheServersNumber();
+		int cacheCapacityInMb = parameters.getCacheCapacityInMb();
+		
+		List<Cache> caches = new ArrayList<>(cacheServersNumber);
+		for (int id = 0; id<cacheServersNumber; id ++) {
+			caches.add(new Cache(id, cacheCapacityInMb));
+		}
+		
+		return new Solution(caches.toArray(new Cache[0]));
 	}
 
 }
