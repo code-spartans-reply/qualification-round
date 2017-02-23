687807e57a1d8bed9239d145bc51e34f7df47210
psychowood <psychowood@gmail.com>
2017-02-23 20:02:05 +0100

    Output renderer

--- src/main/java/com/reply/challenges/hashcode2017/codespartans/qualificationround/utils/OutputRenderer.java
+++ src/main/java/com/reply/challenges/hashcode2017/codespartans/qualificationround/utils/OutputRenderer.java
@@ -5,10 +5,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import com.google.common.io.Files;
+import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Cache;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.ProblemParameters;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Solution;
+import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Video;
 
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -23,9 +28,42 @@ public class OutputRenderer {
 		// Ensure target directory exists:
 		Files.createParentDirs(outputFile.toFile());
 		try (final PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(outputFile.toFile())))) {
+			
+			List<Cache> cleanedCacheList = cleanEmptyCaches(result.getCaches());
+			
+			writer.println(cleanedCacheList.size());
+			
+			for(Cache cache : cleanedCacheList) {
+				writer.print(cache.getId() + " ");
+				List<Video> cachedVideos = cache.getCachedVideos();
+				for (Iterator<Video> iterator = cachedVideos.iterator(); iterator.hasNext();) {
+					Video video = (Video) iterator.next();
+					writer.print(video.getId());
+					if (iterator.hasNext()) {
+						writer.print(" ");
+					}
+				}
+			}
+			
 			// TODO writer.print(result.data)
 			writer.flush();
 		}
 	}
 
+	private static List<Cache> cleanEmptyCaches(Cache[] caches) {
+		List<Cache> cleanedCacheList = new ArrayList<>();
+		if (caches != null) {
+			for (int i = 0; i < caches.length; i++) {
+				if (!caches[i].getCachedVideos().isEmpty()) {
+					cleanedCacheList.add(caches[i]);
+				}
+			}
+		}
+		return cleanedCacheList;		
+	}
+
+
+	
 }
+
+
