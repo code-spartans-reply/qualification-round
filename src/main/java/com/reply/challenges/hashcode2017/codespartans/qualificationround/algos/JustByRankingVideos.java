ce0f3c334e91f7b75bd44ac4fe5876b851b7362b
psychowood <psychowood@gmail.com>
2017-02-26 01:40:51 +0100

    Renamed basic algorithm adding Ranking

--- src/main/java/com/reply/challenges/hashcode2017/codespartans/qualificationround/algos/JustByRankingVideos.java
+++ src/main/java/com/reply/challenges/hashcode2017/codespartans/qualificationround/algos/JustByRankingVideos.java
@@ -1,23 +1,28 @@
 package com.reply.challenges.hashcode2017.codespartans.qualificationround.algos;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Cache;
+import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Endpoint;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.ProblemParameters;
+import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Request;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Solution;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.model.problem.Video;
+import com.reply.challenges.hashcode2017.codespartans.qualificationround.utils.Ranker;
 import com.reply.challenges.hashcode2017.codespartans.qualificationround.utils.SolutionEngine;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class PutVideosAsTheyCome extends SolutionEngine {
+public class JustByRankingVideos extends SolutionEngine {
 
 	@Override
 	public Solution processSolution(ProblemParameters parameters) {
-		log.info("Lets fill caches with the provided videos in the order they were provided");
+		log.info("Start looking for a solution to problem");
 
 		int cacheServersNumber = parameters.getCacheServersNumber();
 		int cacheCapacityInMb = parameters.getCacheCapacityInMb();
@@ -27,17 +32,33 @@ public class PutVideosAsTheyCome extends SolutionEngine {
 			caches.add(new Cache(id, cacheCapacityInMb));
 		}
 
-		List<Video> videos = parameters.getVideos();
+		List<Endpoint> endpoints = Ranker.rankEndpoints(parameters);
+		List<Request> requests = parameters.getRequests();
+		List<Video> videos = Ranker.rankVideos(parameters.videos(), parameters.requests());
 
-		Iterator<Video> vidIterator = videos.iterator();
 		Iterator<Cache> cacheIterator = caches.iterator();
+
+		Map<Integer, Endpoint> endpointMap = new HashMap<>();
+		Map<Integer, Video> videoMap = new HashMap<>();
+
+		for (Endpoint endpoint : endpoints) {
+			endpointMap.put(endpoint.getId(), endpoint);
+		}
+		for (Video video : videos) {
+			videoMap.put(video.getId(), video);
+		}
+
+		log.info("Videos: {}, requests: {}, endpoints: {}, caches: {}",
+				new Object[] { videos.size(), requests.size(), endpoints.size(), caches.size() });
 
 		while (cacheIterator.hasNext()) {
 			Cache cache = cacheIterator.next();
-
+			log.debug("Processing cache {}", cache.getId());
+			Iterator<Video> vidIterator = videos.iterator();
 			while (vidIterator.hasNext()) {
 				Video video = vidIterator.next();
 				if (cache.addCachedVideo(video)) {
+					log.debug("Added video {}", video.getId());
 					vidIterator.remove();
 				}
 			}
@@ -46,5 +67,4 @@ public class PutVideosAsTheyCome extends SolutionEngine {
 
 		return new Solution(caches.toArray(new Cache[0]));
 	}
-
 }
